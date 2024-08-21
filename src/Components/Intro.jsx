c238508bc765b9e6d5f5efa29ca71181b66fb505
LenaDurant <durant.arlena@gmail.com>
2024-08-21 19:38:46 -0400

    updated list items and added h1 styling

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -3,16 +3,25 @@ import React from 'react';
 const Intro = () => {
     return (
       <>
-        <h1>SayLess</h1>
-        <h2>The fun phrase game yo wil e itchg to ply!</h2>
+      
+        <div class="header">
+         <h1>
+          <span>SayLess</span>
+  
+          <span>SayLess</span>
+          <span>SayLess</span>
+         </h1>
+        </div>
+
+        <h2>The fun phrase game you will be itching to play!</h2>
         <h3>SayLess is a game where your opponent attempts to guess an entire phrase correctly by using only half of the characters used in the original phrase.</h3>
         <h4>
           <ol>
-            <div>Player One will create or select a phrase to use.</div>
-            <div>Player One will then reduce the phrase by using only half of the characters of the original phrase.</div>
-            <div>Player One will then send the shortended phrase to Player Two to decipher.</div>
-            <div>Player Two must guess the correct phrase in order to win!</div>
-            <div>Players will earn points based on the amount of characters in the original phrase that was solved correctly!</div>
+            <li>Player One will create or select a phrase to use.</li>
+            <li>Player One will then reduce the phrase by using only half of the characters of the original phrase.</li>
+            <li>Player One will then send the shortended phrase to Player Two to decipher.</li>
+            <li>Player Two must guess the correct phrase in order to win!</li>
+            <li>Players will earn points based on the amount of characters in the original phrase that was solved correctly!</li>
           </ol>
         </h4>
       </>
