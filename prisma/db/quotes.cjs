b4a2d2befe61c3a0e5befc20ecb564ba903d87f5
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-08-23 13:13:54 -0400

    Don't sanitize custom quotes in the backend
    Made a ticket to do it on the front-end

--- prisma/db/quotes.cjs
+++ prisma/db/quotes.cjs
@@ -2,18 +2,10 @@ const prisma = require('./client.cjs');
 
 const createCustomQuote = async(text, userId) => {
   try {
-    
-    lowerCaseText = text.toLowerCase();
-
-    const noPunctuationText = (str) => {
-      return str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~1234567890]/g, '');
-    }
-
-    const sanitizedText = noPunctuationText(lowerCaseText);
 
     await prisma.customQuote.create({
       data: {
-        text: sanitizedText,
+        text: text,
         userId: userId
       }
     })
