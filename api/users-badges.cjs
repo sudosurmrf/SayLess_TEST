bdca93479adf5a8443d88021e7bd96a8a2e44bdb
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-08-26 20:04:12 -0400

    Get a user's badges through an API request

--- api/users-badges.cjs
+++ api/users-badges.cjs
@@ -1,5 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const { getWinBadges, getPlayBadges } = require('../prisma/db/users-badges.cjs');
+
+router.get('/:id', async(req, res, next) => {
+  try {
+    let { id } = req.params;
+    id = Number(id);
+    const winBadges = await getWinBadges(id);
+    const playBadges = await getPlayBadges(id);
+    
+    const winAndPlayBadges = {
+      winBadges,
+      playBadges
+    };
+    
+    res.send(winAndPlayBadges);
+
+  } catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
