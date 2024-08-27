a24dff15bf79782110ce12d5285c0d5a46c80fe1
LenaDurant <durant.arlena@gmail.com>
2024-08-27 18:13:19 -0400

    stash

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,8 +3,30 @@ import React from 'react';
 const Login = () => {
     return (
         <>
+          <form>
+        {
+          //add username for register and login
+            showLogin ? 
+             null :
+             <>
+              
+              <input className="login" value={emailInput} onChange={(event) => { setEmailInput(event.target.value) }} type="email" placeholder="email"/>
+             </>
+        }
+
+        <input className="login" value={usernameInput} onChange={(event) => { setUsernameInput(event.target.value)}} placeholder="username" />
+        <input className="login" value={passwordInput} onChange={(event) => { setPasswordInput(event.target.value) }} type="password" placeholder="password"/>
+        {
+            showLogin ? <button>Log In</button> : <button onClick={register}>Register</button>
+        }
+        </form>
+        {
+            showLogin ?
+                <p>Not a Member? Say Less! <button onClick={() => { setShowLogin(false) }}>Register now!</button></p>:
+                <p>Already a Member? <button onClick={() => { setShowLogin(true) }}>Log In</button> here!</p>
+            }
         </>
-    )
+    );
 }
 
 export default Login;
