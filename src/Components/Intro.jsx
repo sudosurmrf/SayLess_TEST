5a66997a7eec6af359f4f908e07550e08eacd22e
LenaDurant <durant.arlena@gmail.com>
2024-08-29 12:06:09 -0400

    updated rules based on current functions

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -14,11 +14,11 @@ const Intro = () => {
         <h2 className="rules">Rules</h2>
           <h4>
            <ol>
-            <li>Player One will create or select a phrase to use.</li>
-            <li>Player One will then reduce the phrase by using only half of the characters of the original phrase.</li>
-            <li>Player One will then send the shortended phrase to Player Two to decipher.</li>
+            <li>Player One will select a phrase to use.</li>
+            <li>Player One will then reduce the phrase by using only a portion of the characters used in the original phrase.</li>
+            <li>Player One will then send the shortend phrase to Player Two to decipher.</li>
             <li>Player Two must guess the correct phrase in order to win!</li>
-            <li>Players will earn points based on the amount of characters in the original phrase that was solved correctly!</li>
+            <li>Players will earn points for each phrase that was solved correctly!</li>
            </ol>
           </h4>
           <input className="intro-demo" placeholder='Demo Vid'></input>
