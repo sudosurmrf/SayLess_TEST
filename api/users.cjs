041efe554998db5a081254d0655ab101c9bd982e
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-08-28 21:58:38 -0400

    Change log in requests from get to post

--- api/users.cjs
+++ api/users.cjs
@@ -36,7 +36,7 @@ router.post('/register', async(req, res, next) => {
 });
 
 // login
-router.get('/login', async(req, res, next) => {
+router.post('/login', async(req, res, next) => {
   try {
     const { username, password } = req.body;
     console.log('api/users username', username, 'password', password)
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
 
   const logInUser = async () => {
     try {
-      const response = await axios.get(`${import.meta.env.VITE_API_URL}/users/login`, {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/users/login`, {
         username: usernameInput,
         password: passwordInput,
       });
