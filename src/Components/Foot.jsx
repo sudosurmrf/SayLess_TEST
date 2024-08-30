5eaff0d548865f34f394c67b5b82eaec769200ac
Victor <ban.victor728@gmail.com>
2024-08-29 22:43:28 -0400

    added working links to most of footer elements. changed company names with their logos, added classes to them. styled the class so they're small.

--- src/Components/Foot.jsx
+++ src/Components/Foot.jsx
@@ -8,20 +8,19 @@ const Foot = () => {
               <div className="col-md-4">
                 <h5>Quick Links</h5>
                 <ul className="foot flex-column">
-                  <li><a href="#">Home</a></li>
+                  <li><a href="/">Home</a></li>
                   <li><a href="#">About Us</a></li>
-                  <li><a href="#">Contact Us</a></li>
+                  <li><a href="https://www.linkedin.com">Contact Us</a></li>
                   <li><a href="#">Advertise with Us</a></li>
                 </ul>
               </div>
               <div className="col-md-4">
                 <h5>Social Media</h5>
                 <ul className="foot flex-column">
-                  <li><a href="#">X</a></li>
-                  <li><a href="#">Tik Tok</a></li>
-                  <li><a href="#">Facebook</a></li>
-                  <li><a href="#">Instagram</a></li>
-                  
+                  <li><a href="https://x.com/"><img className="footerlink" src="images\footer-links\twitter-logo-2.svg" alt="X logo"/></a></li>
+                  <li><a href="https://www.tiktok.com/"><img className="footerlink" src="images\footer-links\tiktok-logo-4500.svg" alt="X logo"/></a></li>
+                  <li><a href="https://www.facebook.com/"><img className="footerlink" src="images\footer-links\facebook.svg" alt="X logo"/></a></li>
+                  <li><a href="https://www.instagram.com/"><img className="footerlink" src="images\footer-links\instagram-2016-5.svg" alt="X logo"/></a></li>
                 </ul>
               </div>
             </div>
