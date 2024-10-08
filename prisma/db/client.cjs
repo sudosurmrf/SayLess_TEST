f291c625f3cf4b4e02f2e4403d4c6092dee3f6cf
Victor <ban.victor728@gmail.com>
2024-10-08 15:32:04 -0400

    removed passing database_url in here, passed in elsewhere

--- prisma/db/client.cjs
+++ prisma/db/client.cjs
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient(process.env.DATABASE_URL);
+const prisma = new PrismaClient();
 
 module.exports = prisma;
