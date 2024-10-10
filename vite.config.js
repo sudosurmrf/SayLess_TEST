d975c47fb7129fc3e6509b027161d629201eac9e
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-10-10 00:14:25 -0700

    added react back to incorrect changes lol

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       // Proxy API requests in development to your local backend
